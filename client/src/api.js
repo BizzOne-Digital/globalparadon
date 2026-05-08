2bf6e3e0387bac3cfc42da589464ee2e11e394af
BizzOne-Digital <Development@bizzonedigital.com>
2026-05-09 01:06:26 +0500

    connect frontend api

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: 'https://globalparadon-api.vercel.app/api'
+});
 
 api.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('cpUser') || 'null');
-  if (user?.token) config.headers.Authorization = `Bearer ${user.token}`;
+
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
+
   return config;
 });
 
