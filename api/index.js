a2983b1fa71b0edba219ecdae8294faf383af2e9
BizzOne-Digital <Development@bizzonedigital.com>
2026-05-09 00:01:35 +0500

    move api to vercel functions

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+app.use('/api/auth', require('../server/routes/auth'));
+app.use('/api/leads', require('../server/routes/leads'));
+app.use('/api/cases', require('../server/routes/cases'));
+app.use('/api/contact', require('../server/routes/contact'));
+
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+module.exports = app;
